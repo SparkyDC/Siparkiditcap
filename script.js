e36b83ba4f7925e1b9ea067cb528db9eb4a84b66
SparkyDC <alimganovabdunur@gmail.com>
2024-10-21 02:12:00 +0600

    Delete script.js
    New version

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Предотвращает отправку формы
-    alert('Спасибо за ваше сообщение! Мы свяжемся с вами в ближайшее время.');
-    this.reset(); // Сбросить форму
-});
